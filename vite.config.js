aca98e2684996826db06bdf1d56334139f3cacaf
Charlie <tkcharlie09@gmail.com>
2024-10-13 14:09:01 +0100

    attempt to fix error 404

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/ChairosCreations',
+  base: '/ChairosCreations/',
   build: {
     outDir: 'dist',
     rollupOptions:{
       input:{
-        main: '/src/assets/pages/index.html',
+        main: '/index.html',
         products: '/src/assets/pages/products.html',
         contact: '/src/assets/pages/contact.html',
         about: '/src/assets/pages/about.html',
